9b7c77dbc680ceb4b533547c9dfc8234a1279aa0
akifsah <04makifsahin@proton.me>
2024-09-14 21:44:22 +0300

    highlight on yank

--- lua/akif/init.lua
+++ lua/akif/init.lua
@@ -1,2 +1,12 @@
 require("akif.remap")
 require("akif.set")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
